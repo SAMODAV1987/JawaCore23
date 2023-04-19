f483b4b2fdd08ca8d7419fcbe7e78507864a12a1
SAMODAV1987 <dsamo@mail.ru>
2023-04-19 13:10:53 +0400

    first project

---
+++ src/classwork/lesson4/ArrayExample.java
@@ -0,0 +1,12 @@
+package classwork.lesson4;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {2,56,8,9,0};
+        for (int i = 0; i < numbers.length ; i++) {
+            System.out.print(numbers[i] +", ");
+
+
+        }
+    }
+}
